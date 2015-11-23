d7c4844b68c3de5868e5718a3ba804371d326641
Kibrom1 <kibmit@gmail.com>
2015-11-23 16:58:12 -0600

    First Commit

---
+++ w1l1/HelloWorldApp.java
@@ -0,0 +1,16 @@
+package w1l1;
+/**
+ * 
+ * 
+ * 
+ * */
+
+public class HelloWorldApp{
+	public static void main(String[] args){
+
+		System.out.println("Hello World");
+		//System.out.println(5/0);
+		float a = (float) 2.9;
+		System.out.println(a/0);
+	}
+}
